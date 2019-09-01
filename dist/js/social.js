40943ae7607cfd94e37175e7f69b58a9f2904d81
Armanzon <arman.kulenovic@gmail.com>
2019-09-01 02:39:13 +0200

    A lotta changes

--- dist/js/social.js
+++ dist/js/social.js
@@ -2,19 +2,24 @@ const twitter = document.getElementById('twitter');
 const facebook = document.getElementById('facebook');
 const instagram = document.getElementById('instagram');
 const linkedin = document.getElementById('linkedin');
+const logo = document.getElementById('logo');
 
 twitter.addEventListener('click', e => {
-  window.location = 'https://twitter.com/ArmanKulenovic';
+  window.open('https://twitter.com/ArmanKulenovic');
 });
 
 facebook.addEventListener('click', e => {
-  window.location = 'https://www.facebook.com/arman.kulenovic';
+  window.open('https://www.facebook.com/arman.kulenovic');
 });
 
 instagram.addEventListener('click', e => {
-  window.location = 'https://www.instagram.com/armankulenovic/';
+  window.open('https://www.instagram.com/armankulenovic/');
 });
 
 linkedin.addEventListener('click', e => {
-  window.location = 'https://www.linkedin.com/in/arman-kulenovic-28428a192/';
+  window.open('https://www.linkedin.com/in/arman-kulenovic-28428a192/');
+});
+
+logo.addEventListener('click', e => {
+  window.location = 'index.html';
 });
---
+++ dist/js/work.js
@@ -0,0 +1,15 @@
+const sports = document.getElementById('item-6');
+const ledger = document.getElementById('item-8');
+const hotel = document.getElementById('item-4');
+
+sports.addEventListener('click', e => {
+  window.open('https://elastic-cray-a72a8d.netlify.com/');
+});
+
+ledger.addEventListener('click', e => {
+  window.open('https://keen-hermann-de274f.netlify.com/');
+});
+
+hotel.addEventListener('click', e => {
+  window.open('https://infallible-johnson-863779.netlify.com/');
+});
