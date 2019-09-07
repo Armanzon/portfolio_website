825cb3d145e07eda964280c1372e518a7ac182be
Armanzon <arman.kulenovic@gmail.com>
2019-09-07 02:32:54 +0200

    Socials

--- dist/js/social.js
+++ dist/js/social.js
@@ -1,20 +1,5 @@
-const twitter = document.getElementById('twitter');
-const facebook = document.getElementById('facebook');
-const instagram = document.getElementById('instagram');
 const linkedin = document.getElementById('linkedin');
 const logo = document.getElementById('logo');
-
-twitter.addEventListener('click', e => {
-  window.open('https://twitter.com/ArmanKulenovic');
-});
-
-facebook.addEventListener('click', e => {
-  window.open('https://www.facebook.com/arman.kulenovic');
-});
-
-instagram.addEventListener('click', e => {
-  window.open('https://www.instagram.com/armankulenovic/');
-});
 
 linkedin.addEventListener('click', e => {
   window.open('https://www.linkedin.com/in/arman-kulenovic-28428a192/');
