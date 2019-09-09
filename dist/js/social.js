823ccad725a8c7d202dc568bd139dd3f27e900c4
Armanzon <arman.kulenovic@gmail.com>
2019-09-09 12:00:20 +0200

    Github Icon

--- dist/js/social.js
+++ dist/js/social.js
@@ -1,8 +1,13 @@
 const linkedin = document.getElementById('linkedin');
 const logo = document.getElementById('logo');
+const git = document.getElementById('github');
 
 linkedin.addEventListener('click', e => {
   window.open('https://www.linkedin.com/in/arman-kulenovic-28428a192/');
+});
+
+git.addEventListener('click', e => {
+  window.open('https://github.com/Armanzon');
 });
 
 logo.addEventListener('click', e => {
